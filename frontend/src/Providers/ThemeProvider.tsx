088feba5cbb1b25a362a5a282833de6059cd1b10
Patryk Kłosiński <klosu10@gmail.com>
2023-09-14 18:49:18 +0200

    feature: create workflow
    Create workflow for frontend project
    
    Refs: CU-8692puh86

---
+++ frontend/src/Providers/ThemeProvider.tsx
@@ -0,0 +1,24 @@
+import {ReactNode} from "react";
+import {ColorScheme, ColorSchemeProvider, MantineProvider} from "@mantine/core";
+import { useHotkeys, useLocalStorage } from '@mantine/hooks';
+
+export const ThemeProvider = ({children}: {children: ReactNode}) => {
+    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+        key: 'mantine-color-scheme',
+        defaultValue: 'dark',
+        getInitialValueInEffect: true,
+    });
+
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+    useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
+    return(
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider theme={{colorScheme}} withGlobalStyles withNormalizeCSS>
+                {children}
+            </MantineProvider>
+        </ColorSchemeProvider>
+    );
+}

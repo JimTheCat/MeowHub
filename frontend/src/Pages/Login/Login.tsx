d6f58672c9272e8cc3f4e3c927634b522663ccec
Jimmy <klosu10@gmail.com>
2023-09-16 12:21:54 +0200

    feature: Create login page
    Enhance design of the page by some gradient
    
    Refs: CU-8692r34bd

--- frontend/src/Pages/Login/Login.tsx
+++ frontend/src/Pages/Login/Login.tsx
@@ -1,4 +1,16 @@
-import {Button, Card, Center, Container, Divider, Group, PasswordInput, Stack, Text, TextInput} from "@mantine/core";
+import {
+  Box,
+  Button,
+  Card,
+  Center,
+  Container,
+  Divider,
+  Group,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput
+} from "@mantine/core";
 import { ContainerVhVw } from "../../Components/ContainerVhVw";
 import {useForm} from "@mantine/form";
 import {useNavigate} from "react-router-dom";
@@ -20,42 +32,54 @@ export const Login = () => {
 
     return (
       <>
-        <ContainerVhVw vh={100} vw={100}>
+        <ContainerVhVw vh={100} vw={100} >
           <Center h={"inherit"}>
             <Group align={"center"} spacing={300}>
               <Stack spacing="xs" >
-                <Text>MeowHub</Text>
+                <Text size={72} fw={700} variant={"gradient"} gradient={{from: 'pink', to: 'cyan', deg: 45}}>MeowHub</Text>
                 <Text>Find your meme soulmate!</Text>
               </Stack>
-              <Card w={300} radius={"md"} shadow={"md"}> {/*here is login form*/}
-                <form onSubmit={form.onSubmit((values: any) => {
-                  console.log(values);
-                  navigate("/mainpage");
-                })}>
-                  <Stack spacing="xs" align={"stretch"}>
-                    <TextInput
-                      withAsterisk
-                      label={'Email'}
-                      placeholder={'Enter your email'}
-                      icon={<IconAt size={"0.8rem"} />}
-                      {...form.getInputProps('email')}
-                    />
-                    <PasswordInput
-                      withAsterisk
-                      label={'Password'}
-                      placeholder={'Enter your password'}
-                      icon={<IconLock size={"1rem"} /> }
-                      {...form.getInputProps('password')}
-                    />
-                    <Button mt={5} type="submit">Log in</Button>
-                    <Text component={'a'} href={'/'} size={"xs"}>Forgot password?</Text>
-                    <Divider my={"xs"}/>
-                  </Stack>
-                  <Group position={"center"}>
-                    <Button color={'green'} mt={5} onClick={() => {navigate("/mainpage")}}>Sign up</Button>
-                  </Group>
-                </form>
-              </Card>
+              <Box>
+                <Card w={300} radius={"md"} shadow={"md"}> {/*here is login form*/}
+                  <form onSubmit={form.onSubmit((values: any) => {
+                    console.log(values);
+                    navigate("/mainpage");
+                  })}>
+                    <Stack spacing="xs" align={"stretch"}>
+                      <TextInput
+                        withAsterisk
+                        label={'Email'}
+                        placeholder={'Enter your email'}
+                        icon={<IconAt size={"0.8rem"} />}
+                        {...form.getInputProps('email')}
+                      />
+                      <PasswordInput
+                        withAsterisk
+                        label={'Password'}
+                        placeholder={'Enter your password'}
+                        icon={<IconLock size={"1rem"} /> }
+                        {...form.getInputProps('password')}
+                      />
+                      <Button mt={5} type="submit">Log in</Button>
+                      <Text component={'a'} href={'/'} size={"xs"}>Forgot password?</Text>
+                      <Divider my={"xs"}/>
+                    </Stack>
+                    <Group position={"center"}>
+                      <Button color={'green'} mt={5} onClick={() => {navigate("/mainpage")}}>Sign up</Button>
+                    </Group>
+                  </form>
+                </Card>
+                <Box sx ={(theme) => ({
+                  position: 'absolute',
+                  backgroundImage: theme.fn.gradient({from: 'pink', to: 'cyan', deg: 45}),
+                  zIndex: -1,
+                  width: '35%',
+                  height: '40%',
+                  top: '0',
+                  filter: 'blur(500px)',
+
+                })} />
+              </Box>
             </Group>
           </Center>
         </ContainerVhVw>

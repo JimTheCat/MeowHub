68401afa1c35f74a41e772ebb18816e4ce1bf581
Patryk Kłosiński <klosu10@gmail.com>
2023-09-17 16:02:36 +0200

    feature: Add page NotFound
    Add translation for page
    
    Refs: CU-8692r3312

--- frontend/src/Services/Utils/i18nInitializer.tsx
+++ frontend/src/Services/Utils/i18nInitializer.tsx
@@ -2,9 +2,11 @@ import i18next from "i18next";
 // english translations
 import enLogin from "../../Translations/en/login.json";
 import enFooter from "../../Translations/en/footer.json";
+import enNotFound from "../../Translations/en/notfound.json";
 // polish translations
 import plLogin from "../../Translations/pl/login.json";
 import plFooter from "../../Translations/pl/footer.json";
+import plNotFound from "../../Translations/pl/notfound.json";
 
 export const i18nInitializer = () => {
   return i18next.init({
@@ -14,10 +16,12 @@ export const i18nInitializer = () => {
       en: {
         login: enLogin,
         footer: enFooter,
+        notfound: enNotFound,
       },
       pl: {
         login: plLogin,
         footer: plFooter,
+        notfound: plNotFound,
       }
     }
   });

35cf833d9e2e074265912318b6b5016f11b6e74d
Patryk Kłosiński <klosu10@gmail.com>
2023-09-18 20:35:44 +0200

    feature: Add new fonts
    Add global font 'Lato' and logo font
    
    Refs: CU-8692r3cba

--- frontend/src/Providers/ThemeProvider.tsx
+++ frontend/src/Providers/ThemeProvider.tsx
@@ -1,24 +1,27 @@
 import {ReactNode} from "react";
 import {ColorScheme, ColorSchemeProvider, MantineProvider} from "@mantine/core";
-import { useHotkeys, useLocalStorage } from '@mantine/hooks';
+import {useHotkeys, useLocalStorage} from '@mantine/hooks';
 
-export const ThemeProvider = ({children}: {children: ReactNode}) => {
-    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-        key: 'mantine-color-scheme',
-        defaultValue: 'dark',
-        getInitialValueInEffect: true,
-    });
+export const ThemeProvider = ({children}: { children: ReactNode }) => {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'dark',
+    getInitialValueInEffect: true,
+  });
 
-    const toggleColorScheme = (value?: ColorScheme) =>
-        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-    useHotkeys([['mod+J', () => toggleColorScheme()]]);
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
 
-    return(
-        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-            <MantineProvider theme={{colorScheme}} withGlobalStyles withNormalizeCSS>
-                {children}
-            </MantineProvider>
-        </ColorSchemeProvider>
-    );
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{
+        colorScheme: colorScheme,
+        fontFamily: 'Lato, sans-serif',
+      }} withGlobalStyles withNormalizeCSS>
+        {children}
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }

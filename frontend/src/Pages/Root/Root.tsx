30026d2a2acb5f7747d6286669c9a6f67d9bf005
Patryk Kłosiński <klosu10@gmail.com>
2023-09-20 19:08:01 +0200

    feature: Create Password recovery page
    Create page with all requirements
    
    Refs: CU-8692r6nb8

--- frontend/src/Pages/Root/Root.tsx
+++ frontend/src/Pages/Root/Root.tsx
@@ -4,15 +4,16 @@ import { MainPage } from "../MainPage";
 import {Login} from "../Login";
 import {Register} from "../Register";
 import {NotFound} from "../NotFound";
+import {Recovery} from "../Recovery";
 
 export const Root = () => {
   return(
     <Routes>
       <Route path="/" element={ <Login/> }/>
       <Route path="/register" element={ <Register/>} />
+      <Route path="/passwordrecovery" element={ <Recovery/>} />
       <Route element={<Layout/>}>
         <Route path="/mainpage" element={ <MainPage/> }/>
-        {/*<Route path="/admin" element={ <Admin/>} />*/}
       </Route>
       <Route path="*" element={ <NotFound/> }/>
     </Routes>

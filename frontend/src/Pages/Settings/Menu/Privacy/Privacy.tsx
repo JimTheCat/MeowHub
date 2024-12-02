a05193347f7a2791f7c730770fe751947e5a233b
Patryk Kłosiński <klosu10@gmail.com>
2024-12-02 22:03:15 +0100

    chore: Change enters
    Remove additional enters
    
    Refs: CU-8696pcytb
    Signed-off-by: Patryk Kłosiński <klosu10@gmail.com>

--- frontend/src/Pages/Settings/Menu/Privacy/Privacy.tsx
+++ frontend/src/Pages/Settings/Menu/Privacy/Privacy.tsx
@@ -3,15 +3,12 @@ import {Box, Fieldset, NativeSelect} from "@mantine/core";
 export const Privacy = () => {
   return (
     <Box>
-
       <Fieldset legend="Overall">
         {/* Privacy settings */}
         <NativeSelect label={"Who can see your profile?"} data={["Everyone", "Only friends", "Nobody"]}/>
         <NativeSelect label={"Who can see your posts?"} data={["Everyone", "Only friends", "Nobody"]} mt={"md"}/>
         <NativeSelect label={"Who can see your friends?"} data={["Everyone", "Only friends", "Nobody"]} mt={"md"}/>
-
       </Fieldset>
-
     </Box>
   );
 }

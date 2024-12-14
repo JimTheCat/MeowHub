e9a20ec47018d46592b926a5b92f9439e82eb609
Patryk Kłosiński <klosu10@gmail.com>
2024-12-14 16:39:33 +0100

    feature: Create post design
    Create base of Post
    * Create ImageWithSkeleton for faster replace of images with loading.
    * Create DTOs used for data integration with backend.
    
    Refs: CU-86971g2py
    Signed-off-by: Patryk Kłosiński <klosu10@gmail.com>

--- frontend/src/Pages/MainPage/MainPage.tsx
+++ frontend/src/Pages/MainPage/MainPage.tsx
@@ -1,9 +1,34 @@
 import {Box, Title} from "@mantine/core";
+import {useEffect, useState} from "react";
+import api from "../../Services/api.ts";
+import {Post} from "../../Components/Cards/Post";
+import {PostDTO} from "../../Services/DTOs/Post.tsx";
 
 export const MainPage = () => {
+
+  const [posts, setPosts] = useState<PostDTO[] | []>([]);
+
+  useEffect(() => {
+    api.get<PostDTO[]>("/api/posts/get-all").then((response) => {
+      setPosts(response.data);
+    });
+  }, []);
+
     return (
       <Box p={"md"}>
         <Title order={1}>Main page</Title>
+
+        {/*Posts*/}
+        {posts.map((post) => (
+          <Post key={post.ownerLogin} userId={post.ownerLogin} contentHtml={post.content} createdAt={post.createdAt}
+                photosUrls={
+                  // generate 100 random photos
+                  Array.from({length: 4}, () => {
+                    return "https://picsum.photos/seed/" + Math.random() + "/800/2200";
+                  })
+                }
+          />
+        ))}
       </Box>
     )
 }
---
+++ frontend/src/Services/DTOs/Post.tsx
@@ -0,0 +1,5 @@
+export type PostDTO = {
+  content: string;
+  ownerLogin: string;
+  createdAt: string;
+}

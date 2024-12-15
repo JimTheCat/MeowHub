14f1bad7a13ca89722d2b95908eea5b97ab892dd
Patryk Kłosiński <klosu10@gmail.com>
2024-12-15 13:22:22 +0100

    feature: Create AlertProvider
    - Update Mantine from 7.14 -> 7.15
    - Add Mantine notifications dependency
    - Create AlertProvider
    - Add interceptor for api to throw notifications when there's an error
    
    Refs: CU-8697352uj
    Signed-off-by: Patryk Kłosiński <klosu10@gmail.com>

--- frontend/src/Services/api.ts
+++ frontend/src/Services/api.ts
@@ -6,6 +6,12 @@ const api = axios.create({
   withCredentials: true, // Cookie handling
 });
 
+let showError: ((message: string) => void) | null = null;
+
+export const setApiErrorHandler = (handler: (message: string) => void) => {
+  showError = handler;
+};
+
 // Interceptor which adds token to request headers
 api.interceptors.request.use((config) => {
   const csrfToken = CookieExtractor('csrf-token'); // Get csrf token from cookies
@@ -19,6 +25,21 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// Error handler interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (showError) {
+      if (error.response) {
+        showError(error.response.data?.message || 'An error occurred');
+      } else {
+        showError('Network error or server is not reachable');
+      }
+    }
+    return Promise.reject(new Error(error));
+  }
+);
 
 api.defaults.withXSRFToken = true
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/tiptap/styles.css';
+import '@mantine/notifications/styles.css';
 import i18next from "i18next";
 import {createTheme, MantineProvider} from "@mantine/core";
 import {BrowserRouter} from "react-router-dom";
@@ -12,6 +13,7 @@ import {I18nextProvider} from "react-i18next";
 import {i18nInitializer} from "./Services/Utils";
 import {ThemeProvider} from "./Providers/ThemeProvider.tsx";
 import {CookiesPopup} from "./Components/CookiesPopup";
+import {AlertProvider} from "./Providers/AlertProvider.tsx";
 
 i18nInitializer();
 
@@ -25,14 +27,16 @@ const theme = createTheme({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider theme={theme} defaultColorScheme={'dark'}>
-      <ThemeProvider>
-        <BrowserRouter>
-          <I18nextProvider i18n={i18next}>
-            <CookiesPopup/>
-            <App/>
-          </I18nextProvider>
-        </BrowserRouter>
-      </ThemeProvider>
+      <AlertProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <I18nextProvider i18n={i18next}>
+              <CookiesPopup/>
+              <App/>
+            </I18nextProvider>
+          </BrowserRouter>
+        </ThemeProvider>
+      </AlertProvider>
     </MantineProvider>
   </StrictMode>,
 )

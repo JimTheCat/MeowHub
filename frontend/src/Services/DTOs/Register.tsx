275759fcf736e9dfa2ceffae897e299408b2b147
Patryk Kłosiński <klosu10@gmail.com>
2024-12-15 16:14:10 +0100

    feature: Correct register form
    - Adapt register form with new values
    - Correct translations
    - Adapt static gender values
    
    Refs: CU-8697369r5
    Signed-off-by: Patryk Kłosiński <klosu10@gmail.com>

---
+++ frontend/src/Services/DTOs/Register.tsx
@@ -0,0 +1,9 @@
+export type RegisterDTO = {
+  "name": string,
+  "surname": string,
+  "birthdate": string, // Format: "YYYY-MM-DD"
+  "gender": string,
+  "email": string,
+  "login": string,
+  "password": string,
+}

6eb37250d4bc0180244e6eeb6774a3fffa820068
Patryk Kłosiński <klosu10@gmail.com>
2025-01-24 19:50:35 +0100

    feature: Update vite.config.ts
    - Add support of dynamic paths in URI
    
    Refs: CU-8697nvcqu
    Signed-off-by: Patryk Kłosiński <klosu10@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,6 +15,8 @@ export default defineConfig(({mode}) => {
           changeOrigin: true,
         },
       },
+      hmr: true,
+      historyApiFallback: true,
     },
     resolve: {
       alias: {

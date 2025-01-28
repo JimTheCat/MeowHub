8b01fd7dc2a2132e2c1b5af6be00803b8c20f717
Patryk Kłosiński <klosu10@gmail.com>
2025-01-28 18:04:55 +0100

    feature: Adjust settings
    - Add possibility to remove your MeowHub account
    
    Refs: CU-8697q57ev
    Signed-off-by: Patryk Kłosiński <klosu10@gmail.com>

--- frontend/src/Features/Settings/components/Menu/Data/Data.tsx
+++ frontend/src/Features/Settings/components/Menu/Data/Data.tsx
@@ -1,15 +1,56 @@
-import {Box, Button, Fieldset, Tooltip} from "@mantine/core";
+import {Box, Button, Fieldset, Group, Tooltip} from "@mantine/core";
 import {useTranslation} from "react-i18next";
+import api from "../../../../shared/services/api.ts";
+import {useAuthStore} from "../../../../shared/services/authStore.ts";
+import {ModificationModal} from "../../../../shared/components/ModificationModal";
+import {useAlert} from "../../../../../Providers/AlertProvider.tsx";
 
 export const Data = () => {
   const {t} = useTranslation('settings');
+  const auth = useAuthStore();
+  const alert = useAlert();
+
+  const handleAccountRemove = async () => {
+    const response = await api.delete(`/api/settings/account`);
+    if (response.status === 200) {
+      auth.logout();
+      alert.showError({
+        title: t('user.personalization.content.account.alert.title'),
+        message: t('user.personalization.content.account.alert.message'),
+        level: 'INFO',
+        timestamp: new Date().toISOString(),
+      });
+    }
+  };
+
+  const confirmAccountRemoval = () => {
+    ModificationModal({
+      handleAction: () => {
+        handleAccountRemove().catch()
+      },
+      title: t('user.personalization.content.account.modal.title'),
+      buttonConfirmText: t('user.personalization.content.account.modal.confirmButton'),
+      buttonConfirmColor: 'red',
+      buttonCancelText: t('user.personalization.content.account.modal.cancelButton'),
+      childrenContent: (
+        <p>{t('user.personalization.content.account.modal.message')}</p>
+      )
+    });
+  };
 
   return (
     <Box>
       <Fieldset legend={t('user.personalization.content.legend')}>
-        <Tooltip label={t('user.personalization.content.data.tooltip')}>
-          <Button variant="outline" color="blue">{t('user.personalization.content.data.children')}</Button>
-        </Tooltip>
+        <Group gap={'xs'}>
+          <Tooltip label={t('user.personalization.content.data.tooltip')}>
+            <Button variant="outline" color="blue">{t('user.personalization.content.data.children')}</Button>
+          </Tooltip>
+          <Tooltip label={t('user.personalization.content.account.tooltip')}>
+            <Button variant="outline" color="red" onClick={confirmAccountRemoval}>
+              {t('user.personalization.content.account.children')}
+            </Button>
+          </Tooltip>
+        </Group>
       </Fieldset>
     </Box>
   );

a293a3b6abd561e3e752d2c4651b7fdede849058
Patryk Kłosiński <klosu10@gmail.com>
2025-02-09 19:53:45 +0100

    feature: Adjust messenger for matching to websockets
    Refs: CU-8697vkcr9
    Signed-off-by: Patryk Kłosiński <klosu10@gmail.com>

---
+++ frontend/src/Features/Matching/components/Messenger/Messenger.tsx
@@ -0,0 +1,6 @@
+import {GenericMessenger} from "../../../shared/components/GenericMessenger";
+import {mainMatchingChatConfig} from "../../../shared/components/GenericMessenger/consts";
+
+export const MatchingMessenger = () => {
+  return <GenericMessenger config={mainMatchingChatConfig}/>;
+};
---
+++ frontend/src/Features/shared/components/GenericMessenger/consts/index.tsx
@@ -0,0 +1,3 @@
+export {Status} from './Activity';
+export {mainRegularChatConfig, mainMatchingChatConfig} from './Config';
+export type {ChatConfig} from './Config';
